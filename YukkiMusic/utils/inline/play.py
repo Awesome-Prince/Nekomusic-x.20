e3ef580b8567e61fe79bfe7e3d1062ff7866595b
Hmfhacker <95634169+Hmfhacker@users.noreply.github.com>
2022-06-19 11:18:28 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -61,7 +61,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"PanelMarkup {videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url="t.me/TamilChat_Cringe_Guys"
+                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_GROUP}"
             )
         ],
         [
